71c5b7d91e1e3a229ffd7858aaf74c763b2e471a
muimi1 <97732516+muimi1@users.noreply.github.com>
2024-05-28 12:34:20 +0300

    commit all

---
+++ android/app/src/main/kotlin/com/muimi/pepesi1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muimi.pepesi1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,1 @@
+const String googleMapApi = 'AIzaSyBabaVf-ajmPc9sevCQBa6FOpCe3b7MbnA';
